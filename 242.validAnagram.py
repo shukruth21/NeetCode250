e5a40912dc2ff4ca67fd639c73b7ca7230cf9233
shukruth21 <99413764+shukruth21@users.noreply.github.com>
2025-10-27 14:58:32 -0400

    Add explanation for dictionary .get() method
    Added explanation of the .get() method for dictionaries.

--- 242.validAnagram.py
+++ 242.validAnagram.py
@@ -6,3 +6,10 @@ class Solution:
             countS[s[i]]=1+countS.get(s[i],0)
             countT[t[i]]=1+countT.get(t[i],0)
         return countS==countT
+
+
+
+In Python, the .get() method is used to safely retrieve a value from a dictionary.
+.get(key,default)
+key → the key you want to look up
+default → the value to return if the key doesn’t exist in the dictionary
