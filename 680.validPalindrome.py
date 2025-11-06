e56f0bb83725435260b0502ed89b30dfd4e3413a
shukruth21 <99413764+shukruth21@users.noreply.github.com>
2025-11-06 14:50:27 -0500

    Create 680.validPalindrome.py

---
+++ 680.validPalindrome.py
@@ -0,0 +1,32 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            if s[l] != s[r]:
+                skipL = s[l + 1 : r + 1]
+                skipR = s[l : r]
+                return skipL == skipL[::-1] or skipR == skipR[::-1]
+            l, r = l + 1, r - 1
+
+        return True
+
+  class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def is_palindrome(l, r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+
+        l, r = 0, len(s) - 1
+        while l < r:
+            if s[l] != s[r]:
+                return (is_palindrome(l + 1, r) or
+                        is_palindrome(l, r - 1))
+            l += 1
+            r -= 1
+
+        return True
