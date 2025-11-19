1edba5a002a9c5505533c6637172fc439ac12e0e
shukruth21 <99413764+shukruth21@users.noreply.github.com>
2025-11-19 18:12:47 -0500

    Fix initial result value in longest substring function
    Change initial result value from 1 to 0 to handle empty input.

--- 3.longestSubstringWithoutRepeatingChar.py
+++ 3.longestSubstringWithoutRepeatingChar.py
@@ -16,7 +16,7 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         charset=set()
         l=0
-        res=1
+        res=0
         for r in range(len(s)):
             while(s[r] in charset):
                 charset.remove(s[l])
