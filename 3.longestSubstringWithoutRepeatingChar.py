0994832c60fa9190686a04218d2e217699c43164
shukruth21 <99413764+shukruth21@users.noreply.github.com>
2025-11-19 18:11:59 -0500

    Add solution for longest substring without repeating chars
    Implemented a new approach using a character set to find the length of the longest substring without repeating characters.

---
+++ 3.longestSubstringWithoutRepeatingChar.py
@@ -0,0 +1,29 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        mp = {}
+        l = 0
+        res = 0
+
+        for r in range(len(s)):
+            if s[r] in mp:
+                l = max(mp[s[r]] + 1, l)
+            mp[s[r]] = r
+            res = max(res, r - l + 1)
+        return res
+        
+            
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        charset=set()
+        l=0
+        res=1
+        for r in range(len(s)):
+            while(s[r] in charset):
+                charset.remove(s[l])
+                l+=1
+            res= max(res,r-l+1)
+            charset.add(s[r])
+        return res
+        
+            
+        
